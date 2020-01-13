613bbd1f54154859c7f922344a45344b698bb7bb
Ruben van der Linde <rubenvdlinde@gmail.com>
2020-01-13 21:42:04 +0000

    Apply fixes from StyleCI

--- api/src/Controller/DashboardController.php
+++ api/src/Controller/DashboardController.php
@@ -4,35 +4,33 @@
 
 namespace App\Controller;
 
+use App\Service\CommonGroundService;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Service\CommonGroundService;
-
 /**
- * Class DashboardController
- * @package App\Controller
+ * Class DashboardController.
+ *
  * @Route("/dashboard")
  */
 class DashboardController extends AbstractController
 {
-
     /**
      * @Route("/")
      * @Template
      */
-	public function indexAction(Request $request, EntityManagerInterface $em, CommonGroundService $commonGroundService)
+    public function indexAction(Request $request, EntityManagerInterface $em, CommonGroundService $commonGroundService)
     {
-    	$components = $commonGroundService->getComponentList();
-    	
-    	foreach($components as $key=>$component){
-    		//$components[$key] = $commonGroundService->getComponentHealth($key);
-    		$components[$key] = $commonGroundService->getComponentResources($key);
-    	}
+        $components = $commonGroundService->getComponentList();
 
-    	return ["components"=>$components];
+        foreach ($components as $key=>$component) {
+            //$components[$key] = $commonGroundService->getComponentHealth($key);
+            $components[$key] = $commonGroundService->getComponentResources($key);
+        }
+
+        return ['components'=>$components];
     }
 }
