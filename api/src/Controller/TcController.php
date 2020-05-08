e76c4737bb47dc88a48376cec08f54827ea43290
ginokok1996 <ginokok1996@hotmail.com>
2020-05-08 12:55:28 +0200

    Added Task component

---
+++ api/src/Controller/TcController.php
@@ -0,0 +1,102 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use App\Service\CommonGroundService;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use App\Security\User\CommongroundUser;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * Class PdcController
+ * @package App\Controller
+ * @Route("/task")
+ */
+class TcController extends AbstractController
+{
+
+	/**
+	 * @Route("/")
+	 * @Template
+	 */
+	public function indexAction(TranslatorInterface $translator)
+	{
+		$variables = [];
+		$variables['title'] = $translator->trans('tasks');
+		$variables['subtitle'] = $translator->trans('the tasks component holds tasks');
+
+		return $variables;
+	}
+
+    /**
+     * @Route("/tasks")
+     * @Template
+     */
+	public function tasksAction(CommonGroundService $commonGroundService, TranslatorInterface $translator)
+    {
+    	$variables = [];
+    	$variables['title'] = $translator->trans('tasks');
+    	$variables['subtitle'] = $translator->trans('all').' '.$translator->trans('tasks');
+        $variables['resources'] = $commonGroundService->getResourceList(['component'=>'task','type'=>'tasks'])["hydra:member"];
+        return $variables;
+    }
+
+    /**
+     * @Route("/task/{id}")
+     * @Template
+     */
+    public function taskAction(Request $request, CommonGroundService $commonGroundService, TranslatorInterface $translator, $id)
+    {
+
+    	$variables = [];
+
+        // Lets see if we need to create
+        if($id == 'new'){
+            $variables['resource'] = ['@id' => null,'id'=>'new'];
+        }
+        else{
+            $variables['resource'] = $commonGroundService->getResource(['component'=>'tc','type'=>'task','id'=> $id]);        }
+
+        // If it is a delete action we can stop right here
+        if($request->query->get('action') == 'delete'){
+            $commonGroundService->deleteResource($variables['resource']);
+            return $this->redirect($this->generateUrl('app_task_tasks'));
+        }
+
+        $variables['title'] = $translator->trans('task');
+    	$variables['subtitle'] = $translator->trans('save or create a').' '.$translator->trans('task');
+        $variables['organizations'] = $commonGroundService->getResourceList(['component'=>'task','type'=>'organizations'])["hydra:member"];
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+
+            // Passing the variables to the resource
+            $resource = $request->request->all();
+            $resource['@id'] = $variables['resource']['@id'];
+            $resource['id'] = $variables['resource']['id'];
+
+            // If there are any sub data sources the need to be removed below in order to save the resource
+            // unset($resource['somedatasource'])
+
+
+            $variables['resource'] = $commonGroundService->saveResource($resource,(['component'=>'task','type'=>'task','id'=>$id]));
+        }
+
+
+        return $variables;
+    }
+
+
+}
