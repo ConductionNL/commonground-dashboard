1f0a6ab156cb77e2bda9b02002852979613bb748
ginokok1996 <ginokok1996@hotmail.com>
2020-05-13 11:40:30 +0200

    Stuf component added to dashboard

--- api/src/Controller/StufController.php
+++ api/src/Controller/StufController.php
@@ -39,4 +39,62 @@ class StufController extends AbstractController
 
 		return $variables;
 	}
+
+    /**
+     * @Route("/stuf_interfaces")
+     * @Template
+     */
+    public function StufInterfacesAction(CommonGroundService $commonGroundService, TranslatorInterface $translator)
+    {
+        $variables = [];
+        $variables['title'] = $translator->trans('stuf_interfaces');
+        $variables['subtitle'] = $translator->trans('all').' '.$translator->trans('stuf_interfaces');
+        $variables['resources'] = $commonGroundService->getResourceList(['component'=>'stuf','type'=>'stuf_interfaces'])["hydra:member"];
+        return $variables;
+    }
+
+    /**
+     * @Route("/stuf_interfaces/{id}")
+     * @Template
+     */
+    public function StufInterfaceAction(Request $request, CommonGroundService $commonGroundService, TranslatorInterface $translator, $id)
+    {
+
+        $variables = [];
+
+        // Lets see if we need to create
+        if($id == 'new'){
+            $variables['resource'] = ['@id' => null,'id'=>'new'];
+        }
+        else{
+            $variables['resource'] = $commonGroundService->getResource(['component'=>'stuf','type'=>'stuf_interface','id'=> $id]);        }
+
+        // If it is a delete action we can stop right here
+        if($request->query->get('action') == 'delete'){
+            $commonGroundService->deleteResource($variables['resource']);
+            return $this->redirect($this->generateUrl('app_stuf_stuf_interfaces'));
+        }
+
+        $variables['title'] = $translator->trans('stuf_interface');
+        $variables['subtitle'] = $translator->trans('save or create a').' '.$translator->trans('stuf_interface');
+        $variables['organizations'] = $commonGroundService->getResourceList(['component'=>'stuf','type'=>'organizations'])["hydra:member"];
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+
+            // Passing the variables to the resource
+            $resource = $request->request->all();
+            $resource['@id'] = $variables['resource']['@id'];
+            $resource['id'] = $variables['resource']['id'];
+
+            // If there are any sub data sources the need to be removed below in order to save the resource
+            // unset($resource['somedatasource'])
+
+
+            $variables['resource'] = $commonGroundService->saveResource($resource,(['component'=>'stuf','type'=>'stuf_interface','id'=>$id]));
+        }
+
+
+        return $variables;
+    }
 }
