357bc4546baa1539499559cb44616be1057f31a4
ginokok1996 <ginokok1996@hotmail.com>
2020-05-18 12:00:10 +0200

    added labels to vrc

--- api/src/Controller/VrcController.php
+++ api/src/Controller/VrcController.php
@@ -199,4 +199,61 @@ class VrcController extends AbstractController
             return $this->render('vrc/request.html.twig', $variables);
         }
     }
+
+    /**
+     * @Route("/labels")
+     * @Template
+     */
+    public function labelsAction(CommonGroundService $commonGroundService, TranslatorInterface $translator)
+    {
+        $variables = [];
+        $variables['title'] = $translator->trans('labels');
+        $variables['subtitle'] = $translator->trans('all').' '.$translator->trans('labels');
+        $variables['resources'] = $commonGroundService->getResourceList(['component'=>'vrc','type'=>'labels'])["hydra:member"];
+        return $variables;
+    }
+
+    /**
+     * @Route("/labels/{id}")
+     * @Template
+     */
+    public function labelAction(Request $request, CommonGroundService $commonGroundService, TranslatorInterface $translator, $id)
+    {
+
+        $variables = [];
+
+        // Lets see if we need to create
+        if($id == 'new'){
+            $variables['resource'] = ['@id' => null,'id'=>'new'];
+        }
+        else{
+            $variables['resource'] = $commonGroundService->getResource(['component'=>'vrc','type'=>'labels','id'=> $id]);        }
+
+        // If it is a delete action we can stop right here
+        if($request->query->get('action') == 'delete'){
+            $commonGroundService->deleteResource($variables['resource']);
+            return $this->redirect($this->generateUrl('app_vrc_labels'));
+        }
+
+
+        $variables['organizations'] = $commonGroundService->getResourceList(['component'=>'vrc','type'=>'organizations'])["hydra:member"];
+
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+
+            // Passing the variables to the resource
+            $resource = $request->request->all();
+            $resource['@id'] = $variables['resource']['@id'];
+            $resource['id'] = $variables['resource']['id'];
+
+            // If there are any sub data sources the need to be removed below in order to save the resource
+            // unset($resource['somedatasource'])
+
+
+            $variables['resource'] = $commonGroundService->saveResource($resource,(['component'=>'vrc','type'=>'labels','id'=>$id]));
+        }
+
+
+        return $variables;
+    }
 }
