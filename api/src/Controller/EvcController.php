b29a0dec061114ba3aff6a7386526fbeb19a2c26
ginokok1996 <ginokok1996@hotmail.com>
2020-06-10 16:15:18 +0200

    evc

--- api/src/Controller/EvcController.php
+++ api/src/Controller/EvcController.php
@@ -105,16 +105,7 @@ class EvcController extends AbstractController
             if (array_key_exists('installation', $resource)) {
                 $installation = $resource['installation'];
 
-                if($installation['propertyArray'] != ''){
-                    $properties = json_decode($installation['propertyArray']);
-                    foreach($properties as $name => $value){
-                        $property = [];
-                        $property['name'] = $name;
-                        $property['value'] = $value;
-                        $property = $commonGroundService->saveResource($property, ['component' => 'evc', 'type' => 'properties']);
-                        array_push($installation['properties'], $property['@id']);
-                    }
-                }
+
 
                 $installation['cluster'] = $resource['@id'];
                 if (array_key_exists('id', $installation)) {
