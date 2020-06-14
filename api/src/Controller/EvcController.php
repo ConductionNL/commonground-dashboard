147378415aa427f5d06097517d1de64e14fa1900
Ruben van der Linde <ruben@conduction.nl>
2020-06-14 20:42:16 +0200

    Upped limit on installations and templates

--- api/src/Controller/EvcController.php
+++ api/src/Controller/EvcController.php
@@ -68,7 +68,7 @@ class EvcController extends AbstractController
             $variables['resource'] = $commonGroundService->getResource(['component'=>'evc', 'type'=>'clusters', 'id'=>$id]);
             $variables['domains'] = $commonGroundService->getResourceList(['component'=>'evc', 'type'=>'domains'], ['cluster.id'=>$id])['hydra:member'];
             $variables['environments'] = $commonGroundService->getResourceList(['component'=>'evc', 'type'=>'environments'], ['cluster.id'=>$id])['hydra:member'];
-            $variables['installations'] = $commonGroundService->getResourceList(['component'=>'evc', 'type'=>'installations'], ['environment.cluster.id'=>$id])['hydra:member'];
+            $variables['installations'] = $commonGroundService->getResourceList(['component'=>'evc', 'type'=>'installations'], ['environment.cluster.id'=>$id,'limit'=>100])['hydra:member'];
             $variables['components'] = $commonGroundService->getResourceList(['component'=>'evc', 'type'=>'components'])['hydra:member'];
         }
 
