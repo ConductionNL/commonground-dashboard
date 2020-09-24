ef716828135fa990939a1b059dc7a949800c4e37
Robert Zondervan <Robert@conduction.nl>
2020-09-24 16:19:57 +0200

    a bit more reverting

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -18,8 +18,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_user_login")
-     * @Route("/dashboard/login", name="app_user_login2")
+     * @Route("/login", name="app_user_login2")
+     * @Route("/dashboard/login", name="app_user_login")
      * @Template
      */
     public function login(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
@@ -38,8 +38,8 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_user_logout")
-     * @Route("/dashboard/logout", name="app_user_logout2")
+     * @Route("/logout", name="app_user_logout2")
+     * @Route("/dashboard/logout", name="app_user_logout")
      */
     public function logout(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
     {
